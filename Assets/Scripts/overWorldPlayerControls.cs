6a235a45484dda2edec8da783e5cdf0115bdf26f
Max Tribble <mtribble@trinity.edu>
2020-10-27 01:45:17 -0500

    created tilemap palette and test Over World

---
+++ Assets/Scripts/overWorldPlayerControls.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class overWorldPlayerControls : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float speed;
+    
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate () {
+        float mH = Input.GetAxis ("Horizontal");
+        float mV = Input.GetAxis ("Vertical");
+        rb.velocity = new Vector3 (mH * speed, mV * speed, 0);
+    }
+}
