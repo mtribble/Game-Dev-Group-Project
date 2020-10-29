7b13fbb02b6afb31efde4bc988f29615d6c553f4
Max Tribble <mtribble@trinity.edu>
2020-10-29 00:10:12 -0500

    finished test Cave

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Door : MonoBehaviour
+{
+    public string destinationScene;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(destinationScene);
+        }
+    }
+}
