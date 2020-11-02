b0a1af9d04a808a29d382dc17f51c6f320e892f6
Rmccallu <31514883+Rmccallu@users.noreply.github.com>
2020-11-02 14:12:36 -0600

    Added turn order
    Still need to work on enemy AI and GUI for letting players select their actions.

--- CombatManager.cs
+++ CombatManager.cs
@@ -6,12 +6,65 @@ using UnityEngine;
 
 public class CombatManager : MonoBehaviour
 {
-    public class stats (int hp, int maxhp, int str, int def, int mag, int magdef);
+    public class stats (int hp, int maxhp, int str, int def, int mag, int magdef, int speed);
 
                     //Type false for physical, true for magic
     public class attack (bool type, int power);
 
-    public void combat (attack move, stats attacker, stats defender) {
+    public class Character (stats stat, List<attack>);
+
+    //to be included when finished elsewhere: enemy AI for choosing attacks)
+    public class enemy:Character;
+
+    //I'm not sure what we'll put in here that's unique for a player; probably equipment 
+    public class player : Character {
+
+    };
+
+    Public bool run = false;
+
+    public void combat (List <player> players, List <enemy> enemies) {
+        //render players and enemies
+
+        List<player> alivePlayers = players;
+        List<enemy> aliveEnemies = enemies;
+
+        while (run == false || alivePlayers.Count == 0 || aliveEnemies == 0) {
+            List order = turnOrder(alivePlayers, aliveEnemies);
+
+            for (int i == 0; i < order.Count; i++) {
+                current = order(i);
+
+                //have the player or the enemy take their turn 
+            }
+
+
+        }
+
+        if (run == false || alivePlayers.Count == 0) return 0;
+        else return 1;
+
+    }
+
+    public void turnOrder (List <player> players, List <enemy> enemies) {
+        List <Character> allSides = players.Concat(enemies);
+        List <Character> order;
+        while (allSides.Count != 0) {
+            int highSpeed = 0;
+            int location = 1;
+            for (int i = 0; i > allSides.Count; i++) {
+                int current = allSides(i).stats.speed
+                if (current > highSpeed) {
+                    highSpeed = allSides(i).stats.speed;
+                    location = i;
+                }
+            }
+            order.Add(allSides.location);
+            allSides.Remove(location);
+        }
+    }
+
+    public void damageStep (attack move, stats attacker, stats defender) {
         int atk;
         int def;
         if (move.type == false) {
@@ -24,13 +77,11 @@ public class CombatManager : MonoBehaviour
         }
         int damage = move.power * atk - def;
         defender.hp = defender.hp - damage;
+
+
     }
 
-    //to be included when finished elsewhere: enemy AI for choosing attacks)
-    public class enemy (stats stat, List<attack> moves);
-
-    //I'm not sure what we'll put in here that's unique for a player; probably equipment 
-    public class player (stats stat, List<attack> moves);
+   
 
 
 
@@ -48,6 +99,6 @@ public class CombatManager : MonoBehaviour
     //in combat, be able to target yourself. Has the side effect that magic defense reduces healing, but that can be a feature. 
     attack heal(true, -40);
 
-    enemy testenemy( (100, 100, 15, 10, 1, 5), {bash} );
-    player testplayer( (100, 100, 20, 5, 20, 5), {sword, ignite, heal} );
+    enemy testenemy( (100, 100, 15, 10, 1, 5, 10), {bash} );
+    player testplayer( (100, 100, 20, 5, 20, 5, 15), {sword, ignite, heal} );
 }
