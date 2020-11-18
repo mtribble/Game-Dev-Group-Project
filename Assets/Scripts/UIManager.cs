4e7acca294c4e2104e57a02fb1add2281d35d1ca
aroche97 <aroche@trinity.edu>
2020-11-18 11:34:14 -0600

    added combat button scene

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void AttackButton()
+    {
+        public attack();
+    }
+
+    public void HealButton()
+    {
+        public heal();
+    }
+
+    public void MagicAttack()
+    {
+        public magicAttack();
+    }
+
+    public void Run()
+    {
+        public run();
+    }
+    // Update is called once per frame
+ 
